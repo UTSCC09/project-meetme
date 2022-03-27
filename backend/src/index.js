3ebe399f171950a65f22ed40c436819c320e7185
ma-anwar <ma.rizvi.anwar@gmail.com>
2022-03-27 03:49:40 -0400

    Add websocket server

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,20 @@
 /* eslint no-console: ["error", { allow: ["warn"] }] */
 import http from "http";
 import { ExpressPeerServer } from "peer";
+import { WebSocketServer } from "ws";
+import { useServer } from "graphql-ws/lib/use/ws";
 import app from "./app";
-import server from "./server";
+import server, { schema } from "./server";
 import { connectDb } from "./models";
 
 async function startApolloServer(port) {
     const httpServer = http.createServer(app);
+
+    const wsServer = new WebSocketServer({
+        server: httpServer,
+    });
+
+    const serverCleanup = useServer({ schema }, wsServer);
 
     const peerServer = ExpressPeerServer(httpServer, {
         debug: true,
@@ -28,7 +36,7 @@ async function startApolloServer(port) {
     });
     httpServer.listen(port, () => {
         console.log(
-            `🚀 Apollo Server running on http://localhost:${port}/graphql`
+            `🚀 Apollo Server running on http://localhost:${port}${server.graphqlPath}`
         );
     });
 }
