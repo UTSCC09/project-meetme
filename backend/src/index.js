bfafea3312385f030a46adb5b6dced95cf3da908
ma-anwar <ma.rizvi.anwar@gmail.com>
2022-03-27 03:43:03 -0400

    Add peerserver

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,22 @@
 /* eslint no-console: ["error", { allow: ["warn"] }] */
 import http from "http";
+import { ExpressPeerServer } from "peer";
 import app from "./app";
 import server from "./server";
 import { connectDb } from "./models";
 
 async function startApolloServer(port) {
     const httpServer = http.createServer(app);
+
+    const peerServer = ExpressPeerServer(httpServer, {
+        debug: true,
+        path: "/meetme",
+        port,
+        proxied: true,
+    });
+
+    app.use("/peerjs", peerServer);
+
     await server.start();
     server.applyMiddleware({
         app,
