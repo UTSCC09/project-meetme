8740bd362f66e920f28e5c4c5b5f6f31117f6c1b
ma-anwar <ma.rizvi.anwar@gmail.com>
2022-04-06 05:53:12 -0400

    Add paginated eventsOwned query

--- backend/src/user/userSchema.js
+++ backend/src/user/userSchema.js
@@ -4,12 +4,12 @@ export default gql`
     extend type Query {
         me: User!
         user(email: String!): User
+        eventsOwned(email: String!, page: Int): [Event!]
     }
 
     type User {
         _id: ID!
         username: String!
         email: String!
-        eventsOwned: [Event!]
     }
 `;
